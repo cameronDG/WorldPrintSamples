b446ad7427965521ca4a2da8d871bf9a92fec4c6
Cameron Gibson <88738410+cameronDG@users.noreply.github.com>
2021-12-14 12:11:49 -0800

    Update EmailApiController.cs

--- dotnet/EmailApiController.cs
+++ dotnet/EmailApiController.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Sabio.Web.Api.Controllers
+namespace Web.Api.Controllers
 {
     [Route("api/email")]
     [ApiController]
