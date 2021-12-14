37344fac7d04e32048ffcb29a430ad3816db4aad
Cameron Gibson <88738410+cameronDG@users.noreply.github.com>
2021-12-14 10:32:56 -0800

    Add files via upload

---
+++ dotnet/EmailApiController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Sabio.Models.Requests.Email;
+using Sabio.Models.Requests.Tokens;
+using Sabio.Services;
+using Sabio.Web.Controllers;
+using Sabio.Web.Models.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sabio.Web.Api.Controllers
+{
+    [Route("api/email")]
+    [ApiController]
+    public class EmailApiController : BaseApiController
+    {
+        private IEmailService _service = null;
+        private IAuthenticationService<int> _authService = null;
+        public EmailApiController(IEmailService service
+            , ILogger<EmailApiController> logger
+            , IAuthenticationService<int> authService) :base(logger)
+        {
+            _service = service;
+            _authService = authService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<SuccessResponse>> SendEmail(EmailSendRequest email)
+        {
+            int code = 200;
+            BaseResponse response = null;
+
+            try
+            {
+                await _service.SubscriptionEmail(email);
+                response = new SuccessResponse();
+            }
+            catch (Exception ex)
+            {
+                code = 500;
+                response = new ErrorResponse(ex.Message);
+            }
+
+            return StatusCode(code, response);
+        }
+
+        [HttpPost("contactus")]
+        public async Task<ActionResult<SuccessResponse>> ContactUs(EmailSendRequest email)
+        {
+            int code = 200;
+            BaseResponse response = null;
+
+            try
+            {
+                await _service.ContactUs(email);
+
+                response = new SuccessResponse();
+            }
+            catch (Exception ex)
+            {
+
+                Logger.LogError(ex.ToString());
+                response = new ErrorResponse(ex.Message);
+                code = 500;
+            }
+
+            return StatusCode(code, response);
+        }
+
+    }
+}
